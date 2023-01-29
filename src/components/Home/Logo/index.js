f236f4dd6297dcbe225379723131b5f19c4425a1
Glib Kvothe <evakv0th@gmail.com>
2023-01-29 16:20:52 +0200

    3

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -8,31 +8,6 @@ const Logo = () => {
     const outlineLogoRef = useRef()
     const solidLogoRef = useRef()
 
-    useEffect(() => {
-
-        gsap
-            .timeline()
-            .to(bgRef.current, {
-                duration: 1,
-                opacity: 1,
-            })
-            .from(outlineLogoRef.current, {
-                drawSVG: 0,
-                duration: 20,
-            })
-
-        gsap.fromTo(
-            solidLogoRef.current,
-            {
-                opacity: 0,
-            },
-            {
-                opacity: 1,
-                delay: 4,
-                duration: 4,
-            }
-        )
-    }, [])
     return (
         <div className='logo-container'>
             <img ref={solidLogoRef} className='solid-logo' src={LogoS} alt='S' />
